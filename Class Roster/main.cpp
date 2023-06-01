03315f9d918f50c7c7c64565de9f8d750c4db63f
Scipio McCaffrey <jsmccaffrey@gmail.com>
2023-06-01 13:26:03 -0700

    Change course days and add screenshot

--- Class Roster/main.cpp
+++ Class Roster/main.cpp
@@ -33,11 +33,11 @@ int main()
 	// Req A. In file main.cpp add studentData[] from task scenario into the main() function
 	const string studentData[] = 
 	{
-		"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+		"A1,John,Smith,John1989@gm ail.com,20,30,35,42,SECURITY",
+		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,55,NETWORK",
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,James,McCaffrey,jmc1180@wgu.edu,23,35,21,7,SOFTWARE" 
+		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,19,SECURITY",
+		"A5,James,McCaffrey,jmc1180@wgu.edu,23,35,21,26,SOFTWARE" 
 	}; 
 
 	const int numStudents = 5;
